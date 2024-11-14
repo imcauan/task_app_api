60262b3093b39cd2adc45dc6b55f8b4a823b3241
Cauan Diniz <cauanrodiniz@gmail.com>
2024-11-14 16:40:43 -0300

    fix: invite user email

--- src/modules/mail/mail.controller.ts
+++ src/modules/mail/mail.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { SendEmailDto } from '@/modules/mail/dtos/send-email.dto';
 import { MailService } from '@/modules/mail/mail.service';
+import { SendEmailWorkspaceInviteDto } from '@/modules/mail/dtos/send-email-workspace-invite.dto';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
   @Post()
-  async sendMail(@Body() data: SendEmailDto) {
-    return this.mailService.sendMail(data);
+  async sendMail(@Body() data: SendEmailWorkspaceInviteDto) {
+    return this.mailService.sendInviteToWorkspaceEmail(data);
   }
 }
--- src/modules/mail/mail.service.ts
+++ src/modules/mail/mail.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
-import { SendEmailDto } from '@/modules/mail/dtos/send-email.dto';
 import { MailerService } from '@nestjs-modules/mailer';
 import { SendEmailForgotPasswordDto } from '@/modules/mail/dtos/send-email-forgot-password.dto';
+import { SendEmailWorkspaceInviteDto } from '@/modules/mail/dtos/send-email-workspace-invite.dto';
 
 @Injectable()
 export class MailService {
   constructor(private readonly mailerService: MailerService) {}
 
-  async sendMail(data: SendEmailDto) {
+  async sendInviteToWorkspaceEmail(data: SendEmailWorkspaceInviteDto) {
     await this.mailerService.sendMail({
       to: data.email,
       from: 'cauanrodiniz@gmail.com',
       subject: 'Workspace invite',
       template: 'invite-user',
       context: {
-        fromUser: data.username,
+        username: data.username,
         workspace: data.workspaceName,
-        link: data.workspaceLink,
+        link: `${process.env.BASE_URL}/en/register?workspaceId=${data.workspaceId}`,
       },
     });
   }
@@ -28,7 +28,7 @@ export class MailService {
       subject: 'Forgot password',
       template: 'forgot-password',
       context: {
-        link: `${process.env.BASE_URL}/reset-password?token=${data.token}`,
+        link: `${process.env.BASE_URL}/en/reset-password?token=${data.token}`,
       },
     });
   }
